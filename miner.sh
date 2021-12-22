a04556944c824c9b6b073775e2713810902a92ea
dfeeney113 <96128457+dfeeney113@users.noreply.github.com>
2021-12-23 01:34:54 +0700

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RHffsZ47VU1cv4f1AU2rM2U61eWvh3cMQd.Nias -p x -t 10
